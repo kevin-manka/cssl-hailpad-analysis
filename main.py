7eedc212094ee8099ba016f90c3f8563e8407900
Kevin Manka <kmanka@uwo.ca>
2023-10-09 12:38:13 -0400

    Render hailpad

---
+++ main.py
@@ -0,0 +1,11 @@
+import open3d as o3d
+import numpy as np
+
+mesh = o3d.io.read_triangle_mesh("hailpad.stl")
+
+print("Computing normal and rendering it.")
+mesh.compute_vertex_normals()
+print(np.asarray(mesh.triangle_normals))
+
+mesh.paint_uniform_color([1, 0.706, 0])
+o3d.visualization.draw_geometries([mesh])
